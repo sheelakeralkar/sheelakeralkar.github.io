8feed67047d40db513deaf081bc8285538468a88
sheelakeralkar <66865387+sheelakeralkar@users.noreply.github.com>
2020-07-07 14:27:19 +0530

    Delete script4.js

--- module5a/script4.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(event){
- document.querySelector("button").addEventListener("click", function(){
-                   $ajaxUtils.sendGetRequest("/data/name.txt",function(request){
-                   	var name=request.response.Text;
-                   	document.querySelector("content").innerHTML="<h2>Hello " +name + "!</h2>";
-                   });
- });
-});
